30e81582ca5e0a1044dd595d865701cf100f38b3
chrisstanton43 <chrisstanton43@gmail.com>
2017-06-08 10:09:20 +0100

    CssSelectors

---
+++ src/test/java/SeleniumMethods/CssSelectorsLocator.java
@@ -0,0 +1,22 @@
+package SeleniumMethods;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by subbu on 08/06/2017.
+ */
+public class CssSelectorsLocator {
+
+    public static void main(String[] args) {
+        WebDriver driver= new FirefoxDriver();
+
+        driver.get("http://login.salesforce.com/");
+
+        //Syntax for Xpath = //tagname[@attribute='value']
+        //Syntax for Css= tagname[attribute='value']
+        //Here we no need to write the double slaces before the tagname and @ before the attribute
+        driver.findElement(By.cssSelector("input[id='username']")).sendKeys("fewrew");
+    }
+}
